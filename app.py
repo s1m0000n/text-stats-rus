12cf3b6d32bdc5c8dd70c28f20c461c772ed29e8
s1m00n <pogorelcevsa@gmail.com>
2020-10-06 17:18:35 +0300

    Added general stats, morph analysis

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify, request
+from wrappers_tp import HtmlTP
 
 app = Flask(__name__)
 
@@ -11,8 +12,9 @@ def hello_world():
 @app.route('/process')
 def process():
     text = tuple(request.args.items())[0][0]
-    print(text)
-    return jsonify({'sample': 'sample_result'})
+    tp = HtmlTP(text)
+    return jsonify({'morph_analysis_table': tp.morhp_analysis(include_punct=True),
+                    'gen_stat_data': tp.gen_stats()})
 
 
 if __name__ == '__main__':
