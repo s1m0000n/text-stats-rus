b74bb6cb093a79a0f9a036f8aa5dc368121f3f5a
s1m00n <pogorelcevsa@gmail.com>
2020-10-08 04:01:16 +0300

    New feature, performance improvements

--- text_processing.py
+++ text_processing.py
@@ -1,36 +1,30 @@
 from itertools import chain
-
+from heapq import nlargest
 from natasha import MorphVocab, Segmenter, NewsMorphTagger, NewsEmbedding, Doc
 from nltk.corpus import stopwords
 
-_parts_of_speech = {('NOUN',): 'Существительное', ('VERB',): 'Глагол', ('ADJ',): 'Прилагательное',
-                    ('PART', 'AUX',): 'Частица', ('PROPN',): 'Имя собственное', ('DET', 'PRON',): 'Местоимение',
-                    ('ADP', 'SCONJ',): 'Предлог', ('PUNCT',): 'Знак препинания',
-                    ('', 'X',): 'Неизвестное или иноязычное', ('ADV',): 'Наречие', ('INTJ',): 'Междометие',
-                    ('SYM',): 'Символ', ('CCONJ',): 'Союз'}
-
-_parts_of_speech_pl = {('NOUN',): 'Существительные', ('VERB',): 'Глаголы', ('ADJ',): 'Прилагательные',
-                       ('PART', 'AUX',): 'Частицы', ('PROPN',): 'Имена собственные', ('DET', 'PRON'): 'Местоимения',
-                       ('ADP', 'SCONJ',): 'Предлоги', ('PUNCT',): 'Знаки препинания',
-                       ('', 'X',): 'Неизвестные и иноязычные', ('ADV',): 'Наречия', ('INTJ',): 'Междометия',
-                       ('SYM',): 'Символы', ('CCONJ',): 'Союзы'}
-
+parts_of_speech = {('NOUN',): 'Существительное', ('VERB',): 'Глагол', ('ADJ',): 'Прилагательное',
+                   ('PART', 'AUX',): 'Частица', ('PROPN',): 'Имя собственное', ('DET', 'PRON',): 'Местоимение',
+                   ('ADP', 'SCONJ',): 'Предлог', ('PUNCT',): 'Знак препинания',
+                   ('', 'X',): 'Неизвестное или иноязычное', ('ADV',): 'Наречие', ('INTJ',): 'Междометие',
+                   ('SYM',): 'Символ', ('CCONJ',): 'Союз'}
+parts_of_speech_pl = {('NOUN',): 'Существительные', ('VERB',): 'Глаголы', ('ADJ',): 'Прилагательные',
+                      ('PART', 'AUX',): 'Частицы', ('PROPN',): 'Имена собственные', ('DET', 'PRON'): 'Местоимения',
+                      ('ADP', 'SCONJ',): 'Предлоги', ('PUNCT',): 'Знаки препинания',
+                      ('', 'X',): 'Неизвестные и иноязычные', ('ADV',): 'Наречия', ('INTJ',): 'Междометия',
+                      ('SYM',): 'Символы', ('CCONJ',): 'Союзы'}
 cases_translation = {'Nom': 'Именительный',
                      'Gen': 'Родительный',
                      'Dat': 'Дательный',
                      'Acc': 'Винительный',
                      'Ins': 'Творительный',
-                     'Loc': 'Предложный',
-
-                     }
-
+                     'Loc': 'Предложный',}
 cases = cases_translation.keys()
+sw_filter = lambda lst: tuple(filter(lambda x: str(x).lower() not in stopwords.words('russian'), lst))
 
-sw_filter = lambda lst: list(filter(lambda x: str(x).lower() not in stopwords.words('russian'), lst))
 
-
-def find_pos_rus(pos, plural=False):
-    for item in (_parts_of_speech_pl.items() if plural else _parts_of_speech.items()):
+def pos_name_to_rus(pos, plural=False):
+    for item in (parts_of_speech_pl.items() if plural else parts_of_speech.items()):
         if pos in item[0]:
             return item[1]
     return 'Неизвестно'
@@ -40,39 +34,38 @@ class TextProcessing:
     def __init__(self, text):
         self.doc = Doc(text)
         self.doc.segment(Segmenter())
-        emb = NewsEmbedding()
+        self.doc.tag_morph(NewsMorphTagger(NewsEmbedding()))
         morph_vocab = MorphVocab()
-        self.doc.tag_morph(NewsMorphTagger(emb))
-
-        self.words = []
         for token in self.doc.tokens:
             token.lemmatize(morph_vocab)
-            if token.pos not in ('X', 'PUNCT'):
-                self.words.append(token)
-
-        self.tokens_nouns = list(filter(lambda t: t.pos in ['NOUN', 'PROPN'], self.doc.tokens))
-        self.tokens_adjs = list(filter(lambda t: t.pos == 'ADJ', self.doc.tokens))
+        self.words = tuple(filter(lambda x: x.pos not in ('X', 'PUNCT'), self.doc.tokens))
+        self.tokens_nouns = tuple(filter(lambda t: t.pos in ['NOUN', 'PROPN'], self.doc.tokens))
+        self.tokens_adjs = tuple(filter(lambda t: t.pos == 'ADJ', self.doc.tokens))
+        self.tokens_verbs = tuple(filter(lambda t: t.pos == 'VERB', self.doc.tokens))
 
     def unique_lemmas(self, pos=None):
         if pos is None:
-            return list(set([dt.lemma for dt in self.doc.tokens]))
+            return tuple(set(dt.lemma for dt in self.doc.tokens))
         else:
-            return list(set([dt.lemma for dt in filter(lambda dt: dt.pos == pos, self.doc.tokens)]))
+            return tuple(set(dt.lemma for dt in filter(lambda dt: dt.pos == pos, self.doc.tokens)))
 
-    def unique_words(self):
-        return list(set([dt.lemma for dt in self.words]))
+    def unique_words(self, pos = None):
+        if pos is None:
+            return tuple(set(dt.lemma for dt in self.words))
+        else:
+            return tuple(set(dt.lemma for dt in filter(lambda dt: dt.pos == pos, self.words)))
 
     def word_usages(self):
-        return [dt.text for dt in self.words]
+        return tuple(dt.text for dt in self.words)
 
     def token_usages(self):
-        return [dt.text for dt in self.doc.tokens]
+        return tuple(dt.text for dt in self.doc.tokens)
 
     def unique_word_usages(self):
-        return list(set(self.word_usages()))
+        return tuple(set(self.word_usages()))
 
     def unique_token_usages(self):
-        return list(set(self.token_usages()))
+        return tuple(set(self.token_usages()))
 
     def omon_freq_compute(self, include_stopwords=True):
         wu = self.word_usages() if include_stopwords else sw_filter(self.word_usages())
@@ -84,7 +77,7 @@ class TextProcessing:
                 relative = round((absolute / len(self.words if include_stopwords else sw_filter(self.words))) * 100)
                 text = case[0]
                 res.append((text, absolute, relative))
-        return res
+        return tuple(res)
 
     def avg_sent_len(self):
         return round(sum(map(lambda s: len(s.tokens), self.doc.sents)) / len(self.doc.sents))
@@ -97,38 +90,79 @@ class TextProcessing:
 
     def pos_freq_compute(self):
         tokens_by_poses = []
-        for pos in chain(*_parts_of_speech.keys()):
-            tokens_of_pos = list(filter(lambda dt: dt.pos == pos, self.doc.tokens))
-            absolute_wu = len(tokens_of_pos)
-            if absolute_wu != 0:
-                relative_wu = round((absolute_wu / len(self.doc.tokens)) * 100)
-                pos_translated = find_pos_rus(pos, True)
-                unique_lemmas_pos = self.unique_lemmas(pos)
-                absolute_ul = len(unique_lemmas_pos)
-                relative_ul = round((absolute_ul / len(self.unique_lemmas())) * 100)
-                tokens_by_poses.append((absolute_wu, relative_wu, pos, pos_translated, absolute_ul, relative_ul))
-        return tokens_by_poses
+        for pos in chain(*parts_of_speech.keys()):
+            words_of_pos = tuple(filter(lambda dt: dt.pos == pos, self.words))
+            absolute_words_usages = len(words_of_pos)
+            if absolute_words_usages != 0:
+                relative_word_usages = round((absolute_words_usages / len(self.words)) * 100)
+                pos_translated = pos_name_to_rus(pos, True)
+                absolute_unique_words = len(self.unique_words(pos))
+                relative_unique_words = round((absolute_unique_words / len(self.unique_words())) * 100)
+                tokens_by_poses.append((absolute_words_usages, relative_word_usages,
+                                        pos, pos_translated,
+                                        absolute_unique_words, relative_unique_words))
+        return tuple(tokens_by_poses)
 
     def nouns_adj_by_cases(self):
-        nouns = list(filter(lambda t: 'Case' in dict(t.feats).keys(), self.tokens_nouns))
-        adjs = list(filter(lambda t: 'Case' in dict(t.feats).keys(), self.tokens_adjs))
-        result = []
-        for case in cases:
-            result.append((case,
-                           list(filter(lambda t: t.feats['Case'] == case, nouns)),
-                           list(filter(lambda t: t.feats['Case'] == case, adjs))))
-        return result
+        nouns = tuple(filter(lambda t: 'Case' in dict(t.feats).keys(), self.tokens_nouns))
+        adjs = tuple(filter(lambda t: 'Case' in dict(t.feats).keys(), self.tokens_adjs))
+        return tuple((case,
+                      tuple(filter(lambda t: t.feats['Case'] == case, nouns)),
+                      tuple(filter(lambda t: t.feats['Case'] == case, adjs))) for case in cases)
 
     def case_analysis(self):
-        #         ret: [(case, abs_nouns, rel_nouns, abs_adj, rel_adj), ]
         nabc = self.nouns_adj_by_cases()
         result = []
         for i, case in enumerate(cases):
             abs_nouns = len(nabc[i][1])
             abs_adj = len(nabc[i][2])
             rel_nouns = abs_nouns / len(self.tokens_nouns)
+            rel_nouns = round(rel_nouns*100)
             rel_adj = abs_adj / len(self.tokens_adjs)
+            rel_adj = round(100*rel_adj)
+            abs_sum = abs_nouns + abs_adj
+            rel_sum = round((abs_sum / (len(self.tokens_adjs) + len(self.tokens_nouns)))*100)
             result.append((case,
-                           abs_nouns, round(rel_nouns*100),
-                           abs_adj, round(100*rel_adj)))
-        return result
+                           abs_nouns, rel_nouns,
+                           abs_adj, rel_adj,
+                           abs_sum, rel_sum))
+        return tuple(result)
+
+    def verb_form_analysis_tense(self):
+        verbs = tuple(filter(lambda t: 'Tense' in dict(t.feats).keys(), self.tokens_verbs))
+        return tuple((tense, len(tuple(filter(lambda t: t.feats['Tense'] == tense, verbs))))
+                     for tense in ('Past', 'Pres', 'Fut'))
+
+    def verb_form_analysis_person(self):
+        verbs = tuple(filter(lambda t: 'Person' in dict(t.feats).keys(), self.tokens_verbs))
+        return tuple((p, len(tuple(filter(lambda t: t.feats['Person'] == p, verbs))))
+                 for p in ('1', '2', '3'))
+
+    def verb_form_analysis_number(self):
+        verbs = tuple(filter(lambda t: 'Number' in dict(t.feats).keys(), self.tokens_verbs))
+        return tuple((p, len(tuple(filter(lambda t: t.feats['Number'] == p, verbs))))
+                     for p in ('Sing', 'Plur'))
+
+    def simple_summarization(self, top = None):
+        if top is None:
+            top = len(self.doc.sents)*0.15
+            if top < 1:
+                top = 1
+            else:
+                top = round(top)
+        words = sw_filter(self.words)
+        lemma_frequencies = {}
+        for word in words:
+            if word.lemma not in lemma_frequencies.keys():
+                lemma_frequencies[word.lemma] = 1
+            else:
+                lemma_frequencies[word.lemma] += 1
+        max_frequency = max(lemma_frequencies.values())
+        sent_scores = {}
+        for sentence in self.doc.sents:
+            # Cумма относительных частот словоупотреблений для каждого предложения текста
+            sent_scores[sentence.text] = sum(tuple(lemma_frequencies[word.lemma]/max_frequency
+                                             for word in filter(lambda w: w.lemma in lemma_frequencies.keys(),
+                                                                sentence.tokens)))
+        summary_sentences = nlargest(top, sent_scores.items(), key=lambda item: item[1])
+        return tuple(t for t,_ in summary_sentences)
--- wrappers_tp.py
+++ wrappers_tp.py
@@ -1,93 +1,82 @@
 import base64
 from copy import deepcopy
 from io import BytesIO
-
 import matplotlib.pyplot as plt
+from text_processing import TextProcessing, pos_name_to_rus, cases_translation
 
-from text_processing import TextProcessing, find_pos_rus, cases_translation
-
-_td = lambda s: f'<td>{s}</td>'
+td = lambda s: f'<td>{s}</td>'
 p = lambda s: f'<p>{s}</p>'
 br = lambda s: f'{s}<br>'
+translation_keys = {
+    'Animacy': 'Одушевлённость',
+    'Case': 'Падеж',
+    'Gender': 'Род',
+    'Number': 'Число',
+    'Voice': 'Залог',
+    'Mood': 'Наклонение',
+    'Person': 'Лицо',
+    'Tense': 'Время',
+    'Aspect': 'Вид'}
+translation_values = {
+    'Nom': 'Именительный',
+    'Gen': 'Родительный',
+    'Loc': 'Предложный',
+    'Acc': 'Винительный',
+    'Ins': 'Творительный',
+    'Dat': 'Дательный',
+    'Inan': 'Неодушевлённое',
+    'Anim': 'Одушевлённое',
+    'Masc': 'Мужской',
+    'Neut': 'Средний',
+    'Fem': 'Женский',
+    'Sing': 'Единственное',
+    'Plur': 'Множественное',
+    'Mid': 'Пассивный',
+    'Act': 'Активный',
+    'Ind': 'Изъявительное',
+    'Imp': 'Повелительное',
+    '3': '3-е',
+    '2': '2-е',
+    '1': '1-е',
+    'Pres': 'Настоящее',
+    'Fut': 'Будущее',
+    'Past': 'Прошедшее',
+}
+translation_values_aspect = {
+    'Imp': 'Несовершенный',
+    'Perf': 'Совершенный'}
 
 
-def _plt_to_html(fig):
+def fig_to_html(fig):
     tempfile = BytesIO()
     fig.savefig(tempfile, format='png')
     encoded = base64.b64encode(tempfile.getvalue()).decode('utf-8')
     return '<img class="img img-fluid" src=\'data:image/png;base64,{}\'>'.format(encoded)
 
 
-def _table_generator(headings, data):
-    """headings: ['h1','h2',...]
-        data: [('s1','s2',),...]"""
-    headings_repr = '<table class="table"><thead><tr><th scope="col">#</th>' + ''.join(
-        [f'<th>{v}</th>' for v in headings]) + '</tr></thead> <tbody>'
-    result = headings_repr
-    for i, t in enumerate(data):
-        line = ''.join([_td(str(el)) for el in t])
-        result += f'<tr><th scope="row">{i + 1}</th>{line}</tr>'
-    result += '</tbody> </table>'
-    return result
+def table_to_html(headings, data):
+    return '<table class="table"><thead><tr><th scope="col">#</th>' + \
+           ''.join(tuple(f'<th>{v}</th>' for v in headings)) + '</tr></thead> <tbody>' + \
+           ''.join(tuple(f'<tr><th scope="row">{i + 1}</th>' + ''.join(tuple(td(str(el)) for el in t)) +
+                         '</tr>' for i, t in enumerate(data))) + '</tbody> </table>'
 
 
-def feats_parser(feats):
-    feats_translation_names = {
-        'Animacy': 'Одушевлённость',
-        'Case': 'Падеж',
-        'Gender': 'Род',
-        'Number': 'Число',
-        'Voice': 'Залог',
-        'Mood': 'Наклонение',
-        'Person': 'Лицо',
-        'Tense': 'Время',
-        'Aspect': 'Вид',
-    }
-    feats_translation_values = {
-        'Nom': 'Именительный',
-        'Gen': 'Родительный',
-        'Loc': 'Предложный',
-        'Acc': 'Винительный',
-        'Ins': 'Творительный',
-        'Dat': 'Дательный',
-        'Inan': 'Неодушевлённое',
-        'Anim': 'Одушевлённое',
-        'Masc': 'Мужской',
-        'Neut': 'Средний',
-        'Fem': 'Женский',
-        'Sing': 'Единственное',
-        'Plur': 'Множественное',
-        'Mid': 'Пассивный',
-        'Act': 'Активный',
-        'Ind': 'Изъявительное',
-        'Imp': 'Повелительное',
-        '3': '3-е',
-        '2': '2-е',
-        '1': '1-е',
-        'Pres': 'Настоящее',
-        'Fut': 'Будущее',
-        'Past': 'Прошедшее',
-    }
-    feats_translation_values_aspect = {
-        'Imp': 'Несовершенный',
-        'Perf': 'Совершенный',
-    }
-
+def translate_features(feats):
     feats = dict(feats)
     f = deepcopy(feats)
     for key in f.keys():
         if key == 'Aspect':
-            if feats[key] in feats_translation_values_aspect.keys():
-                feats[key] = feats_translation_values_aspect[feats[key]]
+            if feats[key] in translation_values_aspect.keys():
+                feats[key] = translation_values_aspect[feats[key]]
             else:
                 feats.pop(key)
         else:
-            if feats[key] in feats_translation_values.keys():
-                feats[key] = feats_translation_values[feats[key]]
+            if feats[key] in translation_values.keys():
+                feats[key] = translation_values[feats[key]]
             else:
                 feats.pop(key)
-        if key in feats_translation_names.keys() and key in feats.keys():
-            feats[feats_translation_names[key]] = feats.pop(key)
+        if key in translation_keys.keys() and key in feats.keys():
+            feats[translation_keys[key]] = feats.pop(key)
     return feats
 
 
@@ -110,43 +99,46 @@ class HtmlTP(TextProcessing):
   </thead> <tbody>'''
         for i, token in enumerate(tokens):
             # preparing features
-            feats = feats_parser(token.feats)
-            feats_s = '<br>'.join([f'{k}: {v.lower()}' for k, v in feats.items()])
-            result += f'<tr><th scope="row">{i + 1}</th>{_td(token.text)}{_td(find_pos_rus(token.pos) + f" ({token.pos})")}{_td(token.lemma)}{_td(feats_s)}</tr>'
+            feats = translate_features(token.feats)
+            feats_s = ',<br>'.join(tuple(f'{k}: {v.lower()}' for k, v in feats.items()))
+            result += f'<tr><th scope="row">{i + 1}</th>{td(token.text)}{td(pos_name_to_rus(token.pos) + f" ({token.pos})")}{td(token.lemma)}{td(feats_s)}</tr>'
         result += '</tbody> </table>'
         return result
 
     def gen_stats_data(self):
         res = ''
-        for stat in (f'Средняя длина предложений: {self.avg_sent_len()}',):
+        for stat in (f'Всего предложений: {len(self.doc.sents)}',
+                     f'Средняя длина предложений: {self.avg_sent_len()}'):
             res += br(stat)
         return p(res)
 
     def gen_stats_with_punct(self):
         res = ''
         for stat in (f'Общее число словоупотреблений: {self.total_lemma_usages()}',
-                     f'Число различных словоформ: {len(self.unique_lemmas())}'):
+                     f'Число различных словоформ: {len(self.unique_token_usages())}',
+                     f'Число уникальных лемм: {len(self.unique_lemmas())}'):
             res += br(stat)
         return p(res)
 
     def gen_stats_wo_punct(self):
         res = ''
         for stat in (f'Общее число словоупотреблений: {self.total_word_usages()}',
-                     f'Число различных словоформ: {len(self.unique_words())}'):
+                     f'Число различных словоформ: {len(self.unique_word_usages())}',
+                     f'Число разных слов: {len(self.unique_words())}'):
             res += br(stat)
         return p(res)
 
     def pos_freq(self):
-        processed_freqs = [
-            (t[3] + f" ({t[2]})", str(t[0]) + ' раз', str(t[1]) + '%', str(t[4]) + ' лемм', str(t[5]) + '%')
-            for t in sorted(self.pos_freq_compute(), key=lambda x: x[0], reverse=True)]
-        return _table_generator(['Часть речи', 'Абсолютная(словоупотребления)', 'Относительная(словоупотребления)',
-                                 'Абсолютная(слова)', 'Относительная(слова)'], processed_freqs)
+        processed_freqs = tuple(
+            (t[3] + f" ({t[2]})", str(t[0]), str(t[1]) + '%', str(t[4]), str(t[5]) + '%')
+            for t in sorted(self.pos_freq_compute(), key=lambda x: x[0], reverse=True))
+        return table_to_html(('Часть речи', 'Абсолютная(словоупотребления)', 'Относительная(словоупотребления)',
+                              'Абсолютная(слова)', 'Относительная(слова)'), processed_freqs)
 
     def pos_freq_graph(self, use_tokens=True):
         data = sorted(self.pos_freq_compute(), key=lambda x: x[0] if use_tokens else x[4], reverse=True)
-        absolutes = [v[0] if use_tokens else v[4] for v in data]
-        poses = [f'{v[3]} ({v[2]})' for v in data]
+        absolutes = tuple(v[0] if use_tokens else v[4] for v in data)
+        poses = tuple(f'{v[3]} ({v[2]})' for v in data)
         plt.rcParams['ytick.labelsize'] = 12
         fig, ax = plt.subplots()
         fig.set_size_inches(20, 8, forward=True)
@@ -154,41 +146,71 @@ class HtmlTP(TextProcessing):
         ax.set_yticks(poses)
         ax.set_yticklabels(poses)
         ax.invert_yaxis()
-        fig.suptitle('Абсолютная частота(словоупотребления)' if use_tokens else 'Абсолютная частота(слова)', fontsize=20)
-        return _plt_to_html(fig)
-
+        fig.suptitle('Абсолютная частота(словоупотребления)' if use_tokens else 'Абсолютная частота(слова)',
+                     fontsize=20)
+        return fig_to_html(fig)
 
     def omon_freq(self, top=None, include_stopwords=True):
-        processed_freqs = [(t[0], str(t[1]) + ' раз', str(t[2]) + '%') for t in
-                           sorted(self.omon_freq_compute(include_stopwords), key=lambda x: x[1], reverse=True)]
+        processed_freqs = tuple((t[0], str(t[1]), str(t[2]) + '%') for t in
+                           sorted(self.omon_freq_compute(include_stopwords), key=lambda x: x[1], reverse=True))
         if top is not None:
             processed_freqs = processed_freqs[:top]
-        return _table_generator(['Словоформа', 'Абсолютная частота', 'Относительная частота'], processed_freqs)
+        return table_to_html(('Словоформа', 'Абсолютная частота', 'Относительная частота'), processed_freqs)
 
     def case_analysis_table(self):
-        processed_freqs = [(cases_translation[t[0]],
-                            str(t[1]) + ' раз', str(t[2]) + '%',
-                            str(t[3]) + ' раз', str(t[4]) + '%') for t in self.case_analysis()]
-        return _table_generator(
-            ['Падеж', 'Абсолютная(сущ.+им.собств.)', 'Относительная(сущ.+им.собств.)', 'Абсолютная(прилагательные)',
-             'Относительная(прилагательные)'], processed_freqs)
+        processed_freqs = tuple((cases_translation[t[0]],
+                            str(t[1]), str(t[2]) + '%',
+                            str(t[3]), str(t[4]) + '%',
+                            str(t[5]), str(t[6]) + '%') for t in self.case_analysis())
+        return table_to_html(
+            ('Падеж', 'abs(сущ.+им.собств.)', 'rel(сущ.+им.собств.)', 'abs(прилагательные)',
+             'rel(прилагательные)', '&#8721; abs', '&#8721; rel'), processed_freqs)
 
     def case_analysis_graph_nouns(self):
         data = self.case_analysis()
-        labels = [cases_translation[d[0]] for d in data if d[2] != 0]
-        sizes = [d[2] for d in data if d[2] != 0]
+        labels = tuple(cases_translation[d[0]] for d in data if d[2] != 0)
+        sizes = tuple(d[2] for d in data if d[2] != 0)
         fig1, ax1 = plt.subplots()
         ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
         ax1.axis('equal')
         fig1.suptitle('Распределение падежей по существительным и именам собственным')
-        return _plt_to_html(fig1)
+        return fig_to_html(fig1)
 
     def case_analysis_graph_adjs(self):
         data = self.case_analysis()
-        labels = [cases_translation[d[0]] for d in data if d[4] != 0]
-        sizes = [d[4] for d in data if d[4] != 0]
+        labels = tuple(cases_translation[d[0]] for d in data if d[4] != 0)
+        sizes = tuple(d[4] for d in data if d[4] != 0)
         fig1, ax1 = plt.subplots()
         ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
         ax1.axis('equal')
         fig1.suptitle('Распределение падежей по прилагательным')
-        return _plt_to_html(fig1)
+        return fig_to_html(fig1)
+
+    def case_analysis_graph_sum(self):
+        data = self.case_analysis()
+        labels = tuple(cases_translation[d[0]] for d in data if d[6] != 0)
+        sizes = tuple(d[6] for d in data if d[6] != 0)
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+        ax1.axis('equal')
+        fig1.suptitle('Суммарное распределение падежей')
+        return fig_to_html(fig1)
+
+    def verb_form_analysis_tense_table(self):
+        processed_freqs = tuple((translation_values[t[0]], str(t[1])) for t in self.verb_form_analysis_tense())
+        return table_to_html(('Время', 'Количество словоупотреблений'), processed_freqs)
+
+    def verb_form_analysis_person_table(self):
+        processed_freqs = tuple((translation_values[t[0]], str(t[1])) for t in self.verb_form_analysis_person())
+        return table_to_html(('Лицо', 'Количество словоупотреблений'), processed_freqs)
+
+    def verb_form_analysis_number_table(self):
+        processed_freqs = tuple((translation_values[t[0]], str(t[1])) for t in self.verb_form_analysis_number())
+        return table_to_html(('Число', 'Количество словоупотреблений'), processed_freqs)
+
+    def summary(self):
+        return ' '.join(self.simple_summarization())
+
+    def top_sents(self, top=10):
+        sents = tuple((s,) for s in self.simple_summarization(top))
+        return table_to_html(('Предложение',), sents)
